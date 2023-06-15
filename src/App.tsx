f0aa6a2fa55ed6847c3f2b4772951db60304d878
kaung <kaungmyathun7@gmail.com>
2023-06-15 11:09:37 +0630

    use: react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { block, For } from "million/react";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const queryClient = new QueryClient();
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <Todos></Todos>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+function Todos() {
+  const queryClient = useQueryClient();
+  const getTodos = () => {};
+  const postTodo = () => {};
+  const query = useQuery("todos", getTodos);
+  const mutation = useMutation(postTodo, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("todos");
+    },
+  });
+
+  return (
+    <div>
+      <ul>
+        <For each={query}>
+          {({ id, title }, index) => <li key={id}>{title}</li>}
+        </For>
+      </ul>
+      <button
+        onClick={() => {
+          mutation.mutate({
+            id: Date.now(),
+            title: "Hello",
+          });
+        }}
+      >
+        Add Todos
+      </button>
+    </div>
+  );
+}
+
+const BlockApp = block(App);
+
+export default BlockApp;
